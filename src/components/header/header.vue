d225a780fbf6f491aa01164a7a3172df84dfb418
brian <brian@cflab.net>
2018-02-08 00:04:04 +0800

    update header.vue

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="header">
-    <div class="content-wrapper"></div>
+    <div class="content-wrapper">
+      <div class="avatar">
+        <img width="64" height="64" :src="seller.avatar" alt="头像">
+      </div>
+      <div class="seller-content">
+        <div class="title">
+          <span class="brand"></span>
+          <span class="seller-name"></span>
+        </div>
+        <p class="delivery">{{seller.description}}/{{seller.deliveryTime}}分钟送达</p>
+        <div v-if="seller.supports" class="supports">
+          <span class="supports-icon" :class="seller.supports[0].type"></span>
+          <span class="supports-text">{{seller.supports[0].description}}</span>
+        </div>
+      </div>
+      <div v-if="seller.supports" class="support-more-btn">
+        <span class="support-num">{{seller.supports.length}}个</span>
+        <i class="keyboard_arrow_right"></i>
+      </div>
+    </div>
     <div class="bulletin-wrapper"></div>
   </div>
 </template>
@@ -16,6 +35,25 @@
   }
 </script>
 
-<style scoped>
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
+  .header
+    background-color rgba(7, 17, 27, .5)
+    filter blur(10px)
+    .content-wrapper
+      padding 24px 12px 18 24
+      font-size 0
+      .avatar
+        display inline-block
+        vertical-align top
+        img
+          border-radius 2px
+      .seller-content
+        display inline-block
+        padding-left 16px
+        .title
+          padding-top 4px
+          .brand
+            bg-img('brand')
 
 </style>
